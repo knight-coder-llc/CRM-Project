8e28af00451b6be0b3a68a7570e953efe2ea56e5
Brian Kilburn <brian.kilburn2@yahoo.com>
2026-03-28 13:53:48 -0400

    added documentation, implemented delete with cascade.

--- crm/urls.py
+++ crm/urls.py
@@ -9,22 +9,29 @@ urlpatterns = [
     path('accounts/<int:pk>/', views.AccountDetailView.as_view(), name='account_detail'),
     path('accounts/create/', views.AccountCreateView.as_view(), name='account_create'),
     path('accounts/<int:pk>/update/', views.AccountUpdateView.as_view(), name='account_update'),
+    path('accounts/<int:pk>/delete/', views.AccountDeleteView.as_view(), name='account_delete'),
     
     # Contacts
     path('contacts/', views.ContactListView.as_view(), name='contact_list'),
     path('contacts/<int:pk>/', views.ContactDetailView.as_view(), name='contact_detail'),
     path('contacts/create/', views.ContactCreateView.as_view(), name='contact_create'),
     path('contacts/<int:pk>/update/', views.ContactUpdateView.as_view(), name='contact_update'),
+    path('contacts/<int:pk>/delete/', views.ContactDeleteView.as_view(), name='contact_delete'),
     
     # Leads
     path('leads/', views.LeadListView.as_view(), name='lead_list'),
     path('leads/<int:pk>/', views.LeadDetailView.as_view(), name='lead_detail'),
     path('leads/create/', views.LeadCreateView.as_view(), name='lead_create'),
     path('leads/<int:pk>/update/', views.LeadUpdateView.as_view(), name='lead_update'),
+    path('leads/<int:pk>/delete/', views.LeadDeleteView.as_view(), name='lead_delete'),
     
     # Opportunities
     path('opportunities/', views.OpportunityListView.as_view(), name='opportunity_list'),
     path('opportunities/<int:pk>/', views.OpportunityDetailView.as_view(), name='opportunity_detail'),
     path('opportunities/create/', views.OpportunityCreateView.as_view(), name='opportunity_create'),
     path('opportunities/<int:pk>/update/', views.OpportunityUpdateView.as_view(), name='opportunity_update'),
+    path('opportunities/<int:pk>/delete/', views.OpportunityDeleteView.as_view(), name='opportunity_delete'),
+    
+    # Documentation
+    path('documentation/', views.DocumentationView.as_view(), name='documentation'),
 ]
--- crm/views.py
+++ crm/views.py
@@ -42,6 +42,11 @@ class AccountUpdateView(UpdateView):
     fields = '__all__'
     success_url = reverse_lazy('account_list')
 
+class AccountDeleteView(DeleteView):
+    model = Account
+    template_name = 'crm/account_confirm_delete.html'
+    success_url = reverse_lazy('account_list')
+
 # --- Contact Views ---
 class ContactListView(ListView):
     model = Contact
@@ -63,6 +68,11 @@ class ContactUpdateView(UpdateView):
     model = Contact
     template_name = 'crm/contact_form.html'
     fields = '__all__'
+    success_url = reverse_lazy('contact_list')
+
+class ContactDeleteView(DeleteView):
+    model = Contact
+    template_name = 'crm/contact_confirm_delete.html'
     success_url = reverse_lazy('contact_list')
 
 # --- Lead Views ---
@@ -88,6 +98,11 @@ class LeadUpdateView(UpdateView):
     fields = '__all__'
     success_url = reverse_lazy('lead_list')
 
+class LeadDeleteView(DeleteView):
+    model = Lead
+    template_name = 'crm/lead_confirm_delete.html'
+    success_url = reverse_lazy('lead_list')
+
 # --- Opportunity Views ---
 class OpportunityListView(ListView):
     model = Opportunity
@@ -110,3 +125,11 @@ class OpportunityUpdateView(UpdateView):
     template_name = 'crm/opportunity_form.html'
     fields = '__all__'
     success_url = reverse_lazy('opportunity_list')
+
+class OpportunityDeleteView(DeleteView):
+    model = Opportunity
+    template_name = 'crm/opportunity_confirm_delete.html'
+    success_url = reverse_lazy('opportunity_list')
+
+class DocumentationView(TemplateView):
+    template_name = 'crm/documentation.html'
